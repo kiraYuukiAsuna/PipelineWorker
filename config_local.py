3e253fabf8fccf0fa14227ad5fd2f9463b8195e4
kiraYuukiAsuna <kirayuukiasuna@outlook.com>
2025-07-23 18:12:38 +0800

    Update config_local.py

--- config_local.py
+++ config_local.py
@@ -2,11 +2,11 @@ import os
 
 CoreServerURL = "http://localhost:8000"
 
-ImageRootDirectory = "./data"
+ImageRootDirectory = "/home/seele/Desktop/WorkSpace/PipelineWorker"
 ImageTransferTemp = os.path.join(ImageRootDirectory, "transfer_temp")
 ImageProcessedFilesArchive = os.path.join(ImageRootDirectory, "Processed_Files_Archive")
 
-HNDBRootDirectory = "./HNDB"
+HNDBRootDirectory = "/home/seele/Desktop/WorkSpace/PipelineWorker/HNDB"
 SamplePreparationDirectory = os.path.join(HNDBRootDirectory, "HNDB_files", "SamplePreparation")
 Cell_ImageDirectory = os.path.join(HNDBRootDirectory, "Cell_Image")
 ImageArchiveDirectory = os.path.join(HNDBRootDirectory, "PTRSB_DB", "ImageArchive")
